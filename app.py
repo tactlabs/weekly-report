93ab2b13714cd7090f9b06ae596890563a950a95
tarun2407 <tarun.s1997@gmail.com>
2021-11-17 22:39:10 +0530

    complete

--- app.py
+++ app.py
@@ -20,25 +20,22 @@ db = cluster['weekly_report']
 # Collection name
 collection = db['intern_id']
 score = db['report']
-    
-@app.route('/')
-def my_form():
-    
-    return render_template('submit.html')
-
+ 
 
 @app.route("/", methods=["GET", "POST"])
 def submit():
     
-    intern_id       = request.form.get('intern_id')
-    # paper_link  = request.form.get('paper_link')
+    intern       = request.form.get('intern_id')
+    rating        = request.form.get('rating')
+    comments      = request.form.get('review_d')
+
     
     new_report_id = get_last_report_id()
     collection.insert_one(
-        {'intern_id': intern_id, 'report_id':new_report_id })
+        {'intern_id': intern, 'rating': rating, 'comments': comments, 'report_id':new_report_id })
 
     
-    return render_template('submit.html')
+    return render_template('review.html')
 
 def get_last_report_id():
     
@@ -50,55 +47,6 @@ def get_last_report_id():
 
     return last_report_id + 1
 
-
-@app.route('/interns')
-def choose_paper():
-    
-    all_interns = collection.find({})
-    result = []
-    for data in all_interns:
-        del data['_id']
-        result.append(data)
-        
-    
-    return render_template('interns.html', result = result)
-
-
-@app.route("/review/<report_id>", methods=['GET'])
-
-def get_info(report_id):
-    
-    all_interns = collection.find_one({'report_id': int(report_id)})
-    return render_template('review.html' , result = all_interns )
-
-
-@app.route("/review/submit", methods=['POST'])
-def review():
-
-    if request.method == 'POST':
-        
-        report_id        = request.form.get('report_id')
-        
-        rating        = request.form.get('rating')
-        comments      = request.form.get('review_d')
-
-        intern_id= collection.find({'report_id': int(report_id)})
-
-        try:
-
-             for item in intern_id:
-                intern_id = item['report_id']
-                current_data = { 'reference': intern_id, 'rating': rating, 'comments': comments }
-                score.insert_one(current_data)
-                
-                print(current_data)
-        
-        except:
-            
-            return "Intern Doesn't Exist"
-        
-
-    return redirect(f'/review/{report_id}')
 
 
 if __name__ == "__main__":
